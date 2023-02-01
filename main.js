89ae35b66796bc1ada0503a0d36654d823f1aaa6
kitkant <nikitadyshkant@gmail.com>
2023-02-01 22:57:24 +0300

    test3

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express') 
 const cors = require('cors') 
 const bodyParser = require('body-parser') 
-conet uuid =  require('react-uuid') 
+const uuid =  require('react-uuid') 
 
 
 const jsonParser = bodyParser.json()
